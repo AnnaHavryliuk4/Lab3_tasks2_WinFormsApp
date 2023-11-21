d950b816a9572155a52732a43b0d73a3d11834bf
AnnaHavryliuk4 <148319228+AnnaHavryliuk4@users.noreply.github.com>
2023-11-21 10:33:50 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Lab3_tasks2_WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(315, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Відкрити";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 123);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 286);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(476, 123);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(312, 286);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab3_tasks2_WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+       
+        private Image MirrorImage(Image originalImage)
+        {
+            originalImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            return originalImage;
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+         {
+             OpenFileDialog ofd = new OpenFileDialog();
+             ofd.Filter = "Image Files (*.bmp, *.jpg, *.png, *.gif, *.tiff)|*.bmp;*.jpg;*.png;*.gif;*.tiff|All Files(*.*)|*.*";
+             if (ofd.ShowDialog() == DialogResult.OK)
+             {
+                try{
+
+                 Image originalImage = new Bitmap(ofd.FileName);
+                 pictureBox1.Image = new Bitmap(ofd.FileName);
+
+                 Image mirroredImage = MirrorImage(originalImage);
+                 pictureBox2.Image = mirroredImage;
+
+                 string mirroredFilePath = Path.GetDirectoryName(ofd.FileName) + "\\" + Path.GetFileNameWithoutExtension(ofd.FileName) + "_mirrored.gif";
+                 mirroredImage.Save(mirroredFilePath, System.Drawing.Imaging.ImageFormat.Gif);
+
+                 MessageBox.Show("Image mirrored successfully!");
+                }
+                catch
+                {
+                 MessageBox.Show("The selected file cannot be opened!!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+             }
+         }
+
+         
+    }
+}
+        
+
